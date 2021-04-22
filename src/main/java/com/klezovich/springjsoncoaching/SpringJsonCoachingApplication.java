c4c202f8ca628d55a6d3fc4f77ce9bfa977767b6
aklezovics <arturs.klezovics@raisin.com>
2021-04-22 23:24:52 +0200

    Add more stuff

---
+++ src/main/java/com/klezovich/springjsoncoaching/SpringJsonCoachingApplication.java
@@ -0,0 +1,13 @@
+package com.klezovich.springjsoncoaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJsonCoachingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJsonCoachingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/klezovich/springjsoncoaching/SpringJsonCoachingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klezovich.springjsoncoaching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJsonCoachingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
